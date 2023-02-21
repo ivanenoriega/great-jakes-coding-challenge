7ac2d5523341598653ee3101bc609771783797a7
Ivan Noriega <ivanenoriega@gmail.com>
2023-02-21 18:06:56 -0300

    Update Pages Layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,28 @@
-import '@/styles/globals.css'
+import '@/styles/normalize.css'
+import { createTheme, ThemeProvider } from '@mui/material';
+import { AnimatePresence, domAnimation, LazyMotion } from 'framer-motion';
 import type { AppProps } from 'next/app'
+import Layout from '@/components/Layout'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	const CustomFontTheme = createTheme({
+		typography: {
+			h1: {
+				fontSize: '3rem',
+				lineHeight: '3rem',
+			}
+		}
+	});
+	
+	return (
+		<ThemeProvider theme={CustomFontTheme}>
+			<LazyMotion features={domAnimation}>
+				<AnimatePresence initial={false}>
+					<Layout>
+						<Component {...pageProps} />
+					</Layout>
+				</AnimatePresence>
+			</LazyMotion>
+		</ThemeProvider>
+	)
 }
